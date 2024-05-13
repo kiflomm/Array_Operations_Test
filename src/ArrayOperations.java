e6157948a9a6d3a79739017ab79c8c7440beb171
Kiflom Berihu Abay <kiflomberihu2024@gmail.com>
2024-05-13 17:44:17 +0000

    update the ArrayOperations.java

--- src/ArrayOperations.java
+++ src/ArrayOperations.java
@@ -2,12 +2,20 @@ import java.util.*;
 public class ArrayOperations {
 
     public static void main(String[] args) {
-        int choice;
-
+        int choice,searchKey=0;
         Scanner scanner = new Scanner(System.in);
-
+        System.out.print("\n \tEnter an Array of integers to perform some operations on it\n start by entering its size: --> ");
+        int size = scanner.nextInt();
+        int[] numbers = new int[size];
+        System.out.println("Enter the integers (separated by spaces): ");
+        for (int i = 0; i < size; i++) {
+            numbers[i] = scanner.nextInt();
+        }  
+        System.out.print("\033[H\033[2J");              // clears the screen after prompting the array successfuly
+        System.out.flush();
+            System.out.println("OKAY NOW you can do the following operations on the array.\n\tplease choose one option");
         do {
-            System.out.println("\nMenu:");
+            
             System.out.println("1. Search from an array");
             System.out.println("2. Sort an array");
             System.out.println("3. Reverse elements of an array");
@@ -15,28 +23,32 @@ public class ArrayOperations {
             System.out.println("5. Count even numbers");
             System.out.println("6. Find sum of prime numbers");
             System.out.println("0. Exit");
-            System.out.print("Enter your choice: ");
-
-            choice = scanner.nextInt();
-
+            System.out.print("Enter your choice: "); 
+            choice = scanner.nextInt();  
+            if(choice == 1){
+                System.out.print("Enter the number to search : ");
+                searchKey = scanner.nextInt();
+            }
+            System.out.print("\033[H\033[2J");              // clears the screen after displaying the menu
+            System.out.flush();
             switch (choice) {
                 case 1:
-                    searchArray();
+                    searchArray(numbers,searchKey);
                     break;
                 case 2:
-                    sortArray();
+                    sortArray(numbers);
                     break;
                 case 3:
-                    reverseArray();
+                    reverseArray(numbers);
                     break;
                 case 4:
-                    calculateAverage();
+                    calculateAverage(numbers);
                     break;
                 case 5:
-                    countEvenNumbers();
+                    countEvenNumbers(numbers);
                     break;
                 case 6:
-                    sumOfPrimes();
+                    sumOfPrimes(numbers);
                     break;
                 case 0:
                     System.out.println("Exiting program...");
@@ -49,40 +61,39 @@ public class ArrayOperations {
         scanner.close();
     }
 
-    public static boolean searchArray() {
-        boolean found = false;
-
+    public static boolean searchArray(int[] numbers,int searchKey) {
+        boolean found = false; 
         return found;
     }
 
-    public static double[] sortArray() {
+    public static double[] sortArray(int[] numbers) {
         double [] sorted = {};
 
         return sorted;
     }
 
 
-    public static double[] reverseArray() {
+    public static double[] reverseArray(int[] numbers) {
         double [] reversed = {};
 
         return reversed;
     }
 
-    public static double calculateAverage() {
+    public static double calculateAverage(int[] numbers) {
         double average = 0;
 
         return average;
     }
 
-    public static int countEvenNumbers() {
+    public static int countEvenNumbers(int[] numbers) {
         int count = 0;
 
         return count;
-    }
+    } 
 
-    public static int sumOfPrimes() {
+    public static int sumOfPrimes(int[] numbers) {
         int sumOfPrimes = 0;
-
+        
         return sumOfPrimes;
     } 
 }
