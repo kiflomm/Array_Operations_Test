6b489af2999e340fc2b7ca4a08c0b456631c48ca
Kamaez <makdatsegazeab93@gmail.com>
2024-05-15 22:09:49 +0300

    this is a test for valid sort ascending

--- src/ArrayOperationsTest.java
+++ src/ArrayOperationsTest.java
@@ -1,4 +1,5 @@
 import org.junit.*; 
+import static org.junit.Assert.*;
 
 public class ArrayOperationsTest {  
 
@@ -8,6 +9,13 @@ public class ArrayOperationsTest {
 
     }
     //Test - two ========> for ArrayOperationTest.sortArray()
+    @Test
+    public void testValidAscendingSort() {
+    int[] numbers = {5, 2, 8, 1, 3};
+    boolean ascending = true;
+    int[] expected = {1, 2, 3, 5, 8};
 
-
+  int[] actual = ArrayOperations.sortArray(numbers.clone(), ascending);
+  assertArrayEquals(expected, actual);
+}
 }
