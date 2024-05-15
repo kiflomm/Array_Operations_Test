cd49198539510488ac390a626e3a5e378f2fe16a
mchealay <haftemchealay>
2024-05-14 17:27:01 -0700

    Test when the search key is not found in the array

--- src/ArrayOperationsTest.java
+++ src/ArrayOperationsTest.java
@@ -3,6 +3,14 @@ public class ArrayOperationsTest {
     //Test - one ========> for ArrayOperationsTest.searchArray() 
     @Test
     public void searchArrayTest(){  
-
+      //Test when the search key is not found in the array
+            int[] numbers = {1, 2, 3, 4, 5};
+            int searchKey = 6;
+            int[] expectedIndices = {};
+        
+            int[] actualIndices = ArrayOperations.searchArray(numbers, searchKey);
+        
+            Assert.assertArrayEquals(expectedIndices, actualIndices);
+        
     }
 }
