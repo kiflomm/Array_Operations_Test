757f90a16153c3d20a118991f93120b57b228765
Girmayg21 <girmayggw78@gmail.com>
2024-05-15 18:44:22 +0300

    I have added my some test methods

--- src/ArrayOperationsTest.java
+++ src/ArrayOperationsTest.java
@@ -7,7 +7,61 @@ public class ArrayOperationsTest {
     public void searchArrayTest(){  
 
     }
-    //Test - two ========> for ArrayOperationTest.sortArray()
 
+
+
+
+
+    
+    //test for empty arrays
+    @Test
+    public void testReverseArray_EmptyArray() {
+        int[] arr = {};
+        int[] expected = {};
+        Assert.assertArrayEquals(expected, ArrayOperations.reverseArray(arr));
+    }
+
+
+
+    // test for single element array
+    @Test
+    public void testReverseArray_SingleElementArray() {
+        int[] arr = {5};
+        
+        int[] expected = {5};
+        Assert.assertArrayEquals(expected, ArrayOperations.reverseArray(arr));
+    }
+
+
+
+     //test for even number of elemnts
+    @Test
+    public void testReverseArray_EvenNumberOfElements() {
+        int[] arr = {1, 2, 3, 4};
+        int[] expected = {4, 3, 2, 1};
+        Assert.assertArrayEquals(expected,  ArrayOperations.reverseArray(arr));
+    }
+
+
+
+    //test for odd number of elemnets
+    @Test
+    public void testReverseArray_OddNumberOfElements() {
+        int[] arr = {1, 2, 3, 4, 5};
+        
+        int[] expected = {5, 4, 3, 2, 1};
+        Assert.assertArrayEquals(expected, ArrayOperations.reverseArray(arr));
+    }
+
+
+
+     //test for dublicate elements in array
+    @Test
+    public void testReverseArray_DuplicateElements() {
+        int[] arr = {2, 4, 2, 1, 4};
+       
+        int[] expected = {4, 1, 2, 4, 2};
+        Assert.assertArrayEquals(expected,  ArrayOperations.reverseArray(arr));
+    }
 
 }
