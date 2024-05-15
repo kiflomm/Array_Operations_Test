a556ff6e9280697d9b9e74ac368031d45468eddb
kapilegend <kapigere00@gmail.com>
2024-05-15 20:30:22 +0300

    Test added

--- src/ArrayOperationsTest.java
+++ src/ArrayOperationsTest.java
@@ -5,4 +5,16 @@ public class ArrayOperationsTest {
     public void searchArrayTest(){  
 
     }
+
+    @Test
+        public void testCalculateAverage() {
+            int[] numbers = {1, 2, 3, 4, 5};
+            
+            double expectedAverage = 3.0;
+            double actualAverage = ArrayOperations.calculateAverage(numbers);
+            
+            // Assert the calculated average is equal to the expected average
+            Assert.assertEquals(expectedAverage, actualAverage, 0.001);
+
+    }
 }
