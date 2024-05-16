930b32939744864bed69323473080d13445c49b3
Kiflom Berihu Abay <kiflomberihu2024@gmail.com>
2024-05-16 03:32:03 +0000

    add some tests

--- src/ArrayOperationsTest.java
+++ src/ArrayOperationsTest.java
@@ -1,26 +1,6 @@
 import org.junit.*;  
 public class ArrayOperationsTest {
-
-    // Hello every one delete this coment when you add your Tests and put your code above this
-
-    @Test
-    public void testSumOfPrimesEmptyArray() {
-        int[] numbers = new int[0];
-        int expectedSum = 0;
-        int actualSum = ArrayOperations.sumOfPrimes(numbers);
-        Assert.assertEquals(expectedSum, actualSum);
-    }
-
-    @Test
-    public void searchArrayTest(){  
-
-    }
-
-
-
-
-
-    
+    //Tests for the ArrayOperations.reverseArray() starts here
     //test for empty arrays
     @Test
     public void testReverseArray_EmptyArray() {
@@ -28,8 +8,6 @@ public class ArrayOperationsTest {
         int[] expected = {};
         Assert.assertArrayEquals(expected, ArrayOperations.reverseArray(arr));
     }
-
-
 
     // test for single element array
     @Test
@@ -40,17 +18,13 @@ public class ArrayOperationsTest {
         Assert.assertArrayEquals(expected, ArrayOperations.reverseArray(arr));
     }
 
-
-
-     //test for even number of elemnts
+    //test for even number of elemnts
     @Test
     public void testReverseArray_EvenNumberOfElements() {
         int[] arr = {1, 2, 3, 4};
         int[] expected = {4, 3, 2, 1};
         Assert.assertArrayEquals(expected,  ArrayOperations.reverseArray(arr));
     }
-
-
 
     //test for odd number of elemnets
     @Test
@@ -61,18 +35,24 @@ public class ArrayOperationsTest {
         Assert.assertArrayEquals(expected, ArrayOperations.reverseArray(arr));
     }
 
-
-
-     //test for dublicate elements in array
+    //test for dublicate elements in array
     @Test
     public void testReverseArray_DuplicateElements() {
         int[] arr = {2, 4, 2, 1, 4};
        
         int[] expected = {4, 1, 2, 4, 2};
         Assert.assertArrayEquals(expected,  ArrayOperations.reverseArray(arr));
+    } 
+
+    @Test
+    public void testSumOfPrimesEmptyArray() {
+        int[] numbers = new int[0];
+        int expectedSum = 0;
+        int actualSum = ArrayOperations.sumOfPrimes(numbers);
+        Assert.assertEquals(expectedSum, actualSum);
     }
 
-}
+    @Test
     public void testSumOfPrimesNoPrimes() {
         int[] numbers = {4, 6, 8, 10};
         int expectedSum = 0;
@@ -103,4 +83,7 @@ public class ArrayOperationsTest {
         int actualSum = ArrayOperations.sumOfPrimes(numbers);
         Assert.assertEquals(expectedSum, actualSum);
     }
+
+    // Hello everyone delete this comment when you add your Tests and put your code above this
+    
 }
